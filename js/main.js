39eb4bb3add1ddfe00df3f3d223cea1fc8007780
NilverTI <nilvert.i13@gmail.com>
2025-07-15 20:19:33 -0500

    V3.8

--- js/main.js
+++ js/main.js
@@ -136,3 +136,16 @@ document.addEventListener("DOMContentLoaded", () => {
   showSlide(currentSlide);
   setInterval(nextSlide, 7000);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav ul li a");
+
+  links.forEach(link => {
+    link.addEventListener("click", function () {
+      links.forEach(l => l.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+});
+
+
